1723ba8c9cbacbc8ed7cd45913ead385054ab70b
lizasilent <elizavetad@yandex.ru>
2022-11-08 14:15:06 +0200

    slider magic done

--- slider.js
+++ slider.js
@@ -3,8 +3,11 @@
   import Swiper from 'swiper';
   // import Swiper styles
   import 'swiper/css';
+  import './styles/index.css';
 
-
+  // Navigation
+const sliderPrev = document.querySelector('.slider__arrow-prev');
+const sliderNext = document.querySelector('.slider__arrow-next');
 
 const slider = new Swiper('.swiper', {
   direction: 'vertical',
@@ -14,22 +17,49 @@ const slider = new Swiper('.swiper', {
   navigation: false,
 });
 
+
 slider.on('slideChange', function () {
   console.log('slide changed');
+
 });
 
-// Navigation
-const sliderPrev = document.querySelector('.slider__arrow-prev');
-const sliderNext = document.querySelector('.slider__arrow-next');
 
 function handleSlidePrev() {
   slider.slidePrev();
 }
 function handleSlideNext() {
   slider.slideNext();
+
 }
 
 sliderPrev.addEventListener('click', handleSlidePrev);
 sliderNext.addEventListener('click', handleSlideNext);
+sliderNext.addEventListener('click', checkArrow);
+sliderPrev.addEventListener('click', checkArrow);
+
+function showArrow() {
+  if ( (slider.slides.length) < 4 ) {
+sliderNext.classList.add("disabled");
+}
+}
+
+showArrow();
+
+
+function checkArrow() {
+
+if (slider.isEnd) {
+  sliderPrev.classList.remove("disabled");
+  sliderNext.classList.add("disabled");
+}
+else if (slider.isBeginning) {
+  sliderPrev.classList.add("disabled");
+  sliderNext.classList.remove("disabled");
+}
+}
+
+
+
+
 
 export default slider;
