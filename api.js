a49d983569bd40331eede56e48c4c590f84fb3a6
lizasilent <elizavetad@yandex.ru>
2022-11-17 17:24:48 +0200

    debounce

--- api.js
+++ api.js
@@ -16,7 +16,8 @@ const searchForm = document.querySelector(".search__results");
 const loader = document.querySelector(".loader");
 const resultTemplate = document.querySelector(".s-catalog__template");
 const searchInpFull = document.querySelector(".search__input-full");
-const searchMoreText = document.querySelectorAll(".search__more-text");
+const searchMoreText = document.querySelector(".search__more-text");
+const searchMoreLink = document.querySelector(".search__more-link");
 const cardPage = document.querySelector(".card-page");
 const resultsSearchPagination = document.querySelector(".s-catalog__pagination");
 
@@ -224,6 +225,7 @@ function renderPagination(itemsCount, pageSize = RESULTS_PAGE_SIZE) {
 
 function createLink(query) {
   searchMoreText.href = `/search-results.html?search=${query}`;
+  searchMoreLink.href = `/search-results.html?search=${query}`;
 }
 
 function handleFillPopup(data) {
